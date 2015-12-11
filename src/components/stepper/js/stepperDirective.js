f039eca6150dda4ebef7cb73d28950e1409104fd
videege <jbarron@barronsoftware.com>
2015-12-11 14:34:28 -0700

    basic setup, empty feature

---
+++ src/components/stepper/js/stepperDirective.js
@@ -0,0 +1,32 @@
+/**
+ * @ngdoc directive
+ * @name mdStepper
+ * @module material.components.stepper
+ * @restrict E
+ * @description
+ *
+ * Steppers do stuff.
+ * 
+ *
+ */
+
+angular
+  .module('material.components.stepper')
+  .directive('mdStepper', StepperDirective);
+
+function StepperDirective() {
+  return {
+    scope: {
+      selectedIndex: '=?mdSelected'
+    },
+    template: function(element, attr) {
+      attr["$mdStepperTemplate"] = element.html();
+      return '' +
+        '<md-stepper-wrapper>' +
+        '</md-stepper-wrapper>';
+    },
+    controller: 'MdStepperController',
+    controllerAs: '$mdStepperCtrl',
+    bindToController: true
+  };
+}
---
+++ src/components/stepper/stepper.js
@@ -0,0 +1,27 @@
+/**
+ * @ngdoc module
+ * @name material.components.stepper
+ * @description
+ *
+ *  Steppers, created with the `<md-stepper>` directive provide *stepped* navigation with different styles.
+ *  The Stepper component consists of sequential or non-sequential tabs that are aligned horizontally or vertically.
+ *
+ *  Features include support for:
+ *
+ *  - static or dynamic tabs,
+ *  - responsive designs,
+ *  - accessibility support (ARIA),
+ *  - tab pagination,
+ *  - external or internal tab content,
+ *  - focus indicators and arrow-key navigations,
+ *  - programmatic lookup and access to tab controllers, and
+ *  - dynamic transitions through different tab contents.
+ *
+ */
+/*
+ * @see js folder for stepper implementation
+ */
+angular.module('material.components.stepper', [
+  'material.core',
+  'material.components.icon'
+]);
